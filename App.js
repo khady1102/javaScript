515776671a37f40ac75cdf682cf8795163071482
khady1102 <khadykebe279@gmail.com>
2021-05-30 22:55:51 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+//DOM
+
+const touches = [...document.querySelectorAll('.buton')];
+const listekeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+        calculer(valeur)
+    
+})
+
+document.addEventListener('click',(e)=>{
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+    
+})
+
+const calculer = (valeur) =>{
+    if(listekeycode.includes(valeur)){
+       switch (valeur) {
+           case "8":
+               ecran.textContent = "";
+               break;
+            case "13" :
+                const calcul = eval(ecran.textContent)
+                ecran.textContent = calcul;
+                break;
+           default:
+               const indexkeycode = listekeycode.indexOf(valeur);
+               const touche  = touches[indexkeycode];
+               ecran.textContent += touche.innerHTML; 
+               break;
+       }
+    }
+}
+window.addEventListener('error',(e) =>{
+    alert ("Une erreur est survenue dans votre calcule"+e.message);
+})
